bf42f03d268e825a9c13d21f47bcd6c9e9e7b072
Chris Brocklesby <Energist@Energist-Macbook.local>
2018-02-07 16:39:08 +0000

    Update

--- routes.js
+++ routes.js
@@ -4,4 +4,4 @@ app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('listening on port 3000!'))
